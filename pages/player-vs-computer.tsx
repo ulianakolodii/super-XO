da53f91e132ac5f6558430dae26a8c4c79a0eac4
Uliana Kolodii <ulianakolodii@gmail.com>
2023-01-31 17:24:32 +0200

    fix: fix player vs player mode logic

--- pages/player-vs-computer.tsx
+++ pages/player-vs-computer.tsx
@@ -55,17 +55,7 @@ const ComputerVsComputer = () => {
   );
 
   const reset = useCallback(() => {
-    setItems([
-      { id: 0, value: "" },
-      { id: 1, value: "" },
-      { id: 2, value: "" },
-      { id: 3, value: "" },
-      { id: 4, value: "" },
-      { id: 5, value: "" },
-      { id: 6, value: "" },
-      { id: 7, value: "" },
-      { id: 8, value: "" },
-    ]);
+    setItems(defaultItems.slice().map((el) => ({ ...el })));
   }, [setItems]);
 
   const handleReset = useCallback(() => {
--- pages/player-vs-player.tsx
+++ pages/player-vs-player.tsx
@@ -1,7 +1,6 @@
 import { useCallback, useMemo, useState } from "react";
 import { Block } from "../types/Block";
 import Grid from "../components/Grid";
-import { getRandomNumberTo } from "../utils/getRandomNumberTo";
 import { isWinner } from "../utils/isWinner";
 
 const defaultItems: Array<Block> = [
@@ -20,8 +19,8 @@ const PlayerVsPlayer = () => {
   const [items, setItems] = useState(
     defaultItems.slice().map((el) => ({ ...el }))
   );
+
   const [current, setCurrent] = useState("x");
-  const [draw, setDraw] = useState(false);
 
   const availableItems = useMemo(
     () => items.filter((item) => item.value === ""),
@@ -30,41 +29,29 @@ const PlayerVsPlayer = () => {
 
   const winnerX = useMemo(() => isWinner(items, "x"), [items]);
   const winnerO = useMemo(() => isWinner(items, "o"), [items]);
-  const isEnd = useMemo(
-    () => winnerX || winnerO || availableItems.length === 0,
-    [winnerX, winnerO, availableItems]
-  );
 
   const toggleCurrent = () =>
     setCurrent((prevCurrent) => (prevCurrent === "x" ? "o" : "x"));
 
   const handleBoxClick = useCallback(
     ({ id, value }: { id: number; value: string }) => {
-      setItems((prevItems) => {
-        if (isEnd) {
-          return prevItems;
-        }
-        const newItems = [...prevItems];
-        newItems[id].value = current;
-        toggleCurrent();
-        return newItems;
-      });
+      if (value === "") {
+        setItems((prevItems) => {
+          if (winnerX || winnerO) {
+            return prevItems;
+          }
+          const newItems = [...prevItems];
+          newItems[id].value = current;
+          toggleCurrent();
+          return newItems;
+        });
+      }
     },
-    [setItems, isEnd, current]
+    [setItems, current, winnerX, winnerO]
   );
 
   const reset = useCallback(() => {
-    setItems([
-      { id: 0, value: "" },
-      { id: 1, value: "" },
-      { id: 2, value: "" },
-      { id: 3, value: "" },
-      { id: 4, value: "" },
-      { id: 5, value: "" },
-      { id: 6, value: "" },
-      { id: 7, value: "" },
-      { id: 8, value: "" },
-    ]);
+    setItems(defaultItems.slice().map((el) => ({ ...el })));
     setCurrent("x");
   }, [setItems, setCurrent]);
 
@@ -79,7 +66,7 @@ const PlayerVsPlayer = () => {
         onClick={handleBoxClick}
         winnerX={winnerX}
         winnerO={winnerO}
-        draw={draw}
+        draw={availableItems.length === 0}
         handleReset={handleReset}
       ></Grid>
     </>
